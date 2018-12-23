2605db651f484df454298f414111a0937edafea5
DBarton <42483562+DylanBarton1856@users.noreply.github.com>
2018-12-22 18:44:24 -0700

    cleanup

--- assets/app.js
+++ assets/app.js
@@ -1,31 +1,31 @@
-// auth key
-var authKey = "ZTWSATUDOEFRKTLCTYET";
+// auth key EVENTBRITE
+var authEventKey = 'ZTWSATUDOEFRKTLCTYET';
 
 // query URL for EVENTBRITE
-var queryURLBase = "https://www.eventbriteapi.com/v3/events/search/?token=" + authKey + "&location.address=phoenix";
+var queryURLBase = 'https://www.eventbriteapi.com/v3/events/search/?token=' + authEventKey + '&location.address=phoenix';
 
 // variable to track number of events
 var eventCounter = 0;
 
 // functions
-function runEventQuery(numArticles, queryURL) {
+function runEventQuery(numEvents, queryURL) {
     // ajax call
-    $.ajax({url: queryURL, method: "GET"})
-    .done(function(eventData) {
-        console.log(queryURL)
-        console.log(eventData); 
-        console.log(eventData.events[1].name);
-        $("#results").text(eventData.events[1].name.text);
-        $("#results").text(eventData.events[1].description.text);
-    })
+    $.ajax({ url: queryURL, method: 'GET' })
+        .done(function (eventData) {
+            console.log(queryURL)
+            console.log(eventData);
+            console.log(eventData.events[1].name);
+            // $("#results").text(eventData.events[1].name.text);
+            $("#results").text(eventData.events[1].description.text);
+        })
 };
 
 // onclick function for searching the EVENTBRITE query
-$('#search').on('click', function() {
+$('#search').on('click', function () {
 
     var queryTerm = $('#search').val().trim();
     console.log(queryTerm);
-    
+
     var newUrl = queryURLBase + "&q=" + queryTerm;
     console.log(newUrl);
 
@@ -33,3 +33,7 @@ $('#search').on('click', function() {
 
     return false;
 });
+
+
+// query URL for WEATHER
+var queryWeatherURL = 'http://api.openweathermap.org/data/2.5/forecast?id=5308655&APPID=3e6d467ec628733e79187ab3ed77a3d8&units=imperial';
