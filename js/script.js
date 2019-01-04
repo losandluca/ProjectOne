f39bcd709349d975fe5ce62f611db20475f47d55
GitDVbootcamp <d_vasquez88@hotmail.com>
2019-01-03 18:41:07 -0700

    updated project

--- js/script.js
+++ js/script.js
@@ -43,12 +43,24 @@ $('#search').on('click', function() {
     return false;
 })
 
+//for (var i = 0; i < numArticles; i++) {
+    // Get specific article info for current index
+    //var article = NYTData.response.docs[i];
+
+    // Increase the articleCount (track article # - starting at 1)
+    //var articleCount = i + 1;
+
+    // Create the  list group to contain the articles and add the article content for each
+   // var $articleList = $("<ul>");
+    //$articleList.addClass("list-group");
+
+    // Add the newly created element to the DOM
+    //$("#article-section").append($articleList);
+
 //1. retrieve user inputs and convert to variables
 //2. use those variables to run an ajax call to eventbrite
 //3. break down eventbrite object into useable fields
 //4. dynamically generate html content
 
 //5. dealing with bugs
-
-
 
