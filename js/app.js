fad3357a53b3224f3823bd80e1da5226d632590a
DBarton <42483562+DylanBarton1856@users.noreply.github.com>
2019-01-06 11:04:26 -0700

    fixes to firebase table

--- js/app.js
+++ js/app.js
@@ -6,51 +6,59 @@ var config = {
     projectId: "groupprojectone-12d0c",
     storageBucket: "groupprojectone-12d0c.appspot.com",
     messagingSenderId: "866266191637"
-  };
-  firebase.initializeApp(config);
+};
+firebase.initializeApp(config);
 
-  var wallPageDatabase = firebase.database();
+var wallPageDatabase = firebase.database();
 
-  $("#add-info-button").on("click", function(){
+
+// onclick function for user attending event!
+$("#add-info-button").on("click", function () {
+    event.preventDefault();
 
     var homeTown = $("#home-town").val().trim();
     var plannedEvent = $("#planned-event").val().trim();
     var partySize = $("#party-size").val().trim();
 
     var informationAdded = {
-        home : homeTown,
-        event : plannedEvent,
-        size : partySize
-  };
+        home: homeTown,
+        event: plannedEvent,
+        size: partySize
+    };
+    // push info to firebase
+    wallPageDatabase.ref().push(informationAdded);
 
-wallPageDatabase.ref().push(informationAdded);
+    console.log(homeTown);
+    console.log(plannedEvent);
+    console.log(partySize);
 
-console.log(homeTown);
-console.log(plannedEvent);
-console.log(partySize);
+    // alert("Your information has been added"); no alerts
 
-alert("Your information has been added");
+    $("#home-town").val("");
+    $("#planned-event").val("");
+    $("#party-size").val("");
 
-$("#home-town").val("");
-$("#planned-event").val("");
-$("#party-size").val("");
-
-return false;
+    return false;
 });
 
-wallPageDatabase.ref().on("child_added", function(childSnapshot, prevChildKey){
-    console.log(childSnapshot.val());
+wallPageDatabase.ref().on("child_added", function (snapshot) {
+    console.log(snapshot.val());
+    var sv = snapshot.val();
 
-    var tableHomeTown = childSnapshot.val().home
-    var tableEvent = childSnapshot.val().event
-    var tableSize = childSnapshot.val().size
+    var tableHomeTown = sv.home
+    var tableEvent = sv.event
+    var tableSize = sv.size
 
-console.log("home", tableHomeTown);
-console.log("event",tableEvent);
-console.log("size",tableSize);
+    console.log("home", tableHomeTown);
+    console.log("event", tableEvent);
+    console.log("size", tableSize);
 
-$("#wall-table > tbody").append("<tr><td>" + tableHomeTown + 
-"</td><td>" + tableEvent + "</td><td>" + tableSize + "</td><tr>");
+    // append firebase data to table
+    $("#wall-table").append(
+        "<tr><td>" + tableHomeTown +
+        "</td><td>" + tableEvent +
+        "</td><td>" + tableSize +
+        "</td><tr>");
 });
 
 // auth key EVENTBRITE
@@ -86,11 +94,11 @@ $(document).ready(function () {
     $eventsTab.click(() => selectPage($eventsPage));
     $weatherTab.click(() => selectPage($weatherPage));
     $wallTab.click(() => selectPage($wallPage));
-    
+
     //Always start on event page
     selectPage($eventsPage);
     $('.slider').slider({ full_width: true });
-    
+
 });
 
 
@@ -108,68 +116,68 @@ function selectPage($selectedPage) {
 // EventBRITE function
 function runEventQuery(queryTerm) {
     var queryURL = 'https://www.eventbriteapi.com/v3/events/search/?q=' + queryTerm + '&sort_by=best&location.address=phoenix&location.within=25mi&token=' + authEventKey;
-    
+
     //date&location.address=phoenix
 
     // ajax call for EventBRITE
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
         console.log(response);
         $resultsContainer.empty();
         response.events.forEach((eventBriteEvent) => {
-                var logoURL= eventBriteEvent.logo.original.url;
-                var image = $("<img>").attr("src", logoURL);
-                var eventName = eventBriteEvent.name.text;
+            var logoURL = eventBriteEvent.logo.original.url;
+            var image = $("<img>").attr("src", logoURL);
+            var eventName = eventBriteEvent.name.text;
 
-                var eventDesc = eventBriteEvent.description.text;
-                //start
-                var eventStart = eventBriteEvent.start.local;
-                var eventStartIn = moment(eventStart).fromNow();
-                var eventStartShort = moment(eventStart).format('dddd [,] MMM Do');
-                var eventStartTime = moment(eventStart).format('h:mm:ss a');
+            var eventDesc = eventBriteEvent.description.text;
+            //start
+            var eventStart = eventBriteEvent.start.local;
+            var eventStartIn = moment(eventStart).fromNow();
+            var eventStartShort = moment(eventStart).format('dddd [,] MMM Do');
+            var eventStartTime = moment(eventStart).format('h:mm:ss a');
 
-                //end
-                var eventEnd = eventBriteEvent.end.local;
-                var eventEndShort = moment(eventEnd).format('dddd, MMM Do');
-                var eventEndTime = moment(eventEnd).format('h:mm:ss a');
+            //end
+            var eventEnd = eventBriteEvent.end.local;
+            var eventEndShort = moment(eventEnd).format('dddd, MMM Do');
+            var eventEndTime = moment(eventEnd).format('h:mm:ss a');
 
-                //url
-                var ebClick = eventBriteEvent.url;
-
-
+            //url
+            var ebClick = eventBriteEvent.url;
 
 
-                var $col = $("<div/>", { class: "col s12 m7" }),
-                    $card = $("<div/>", { class: "card" }),
-                    $cardImg = $("<div/>", { class: "card-image"}),
-                    $cardContent = $("<div/>", { class: "card-content"});
 
 
-                $col.append($card);
-                
-                $cardImg.append(image); 
-                $cardImg.append("<span class='card-title'>" + eventName + "</span>");
-                $cardContent.append("<p><b>Start : </b>" + eventStartIn + "<br>" + eventStartShort + "<b> at </b>" + eventStartTime + "</p>");
-                $cardContent.append("<p><b>End : </b><br>" + eventEndShort + "<b> at </b>" + eventEndTime + "</p>");
-                $cardContent.append("<p><b>Link : </b><span><a href='" + ebClick + "'>Go to Eventbrite</a></span>");
-                $cardContent.append("<p class='comment'><b>Description : </b>" + eventDesc + "</p>");
-                $(".comment").shorten();
-                $card.append($cardImg);
-                $card.append($cardContent);
+            var $col = $("<div/>", { class: "col s12 m7" }),
+                $card = $("<div/>", { class: "card" }),
+                $cardImg = $("<div/>", { class: "card-image" }),
+                $cardContent = $("<div/>", { class: "card-content" });
 
-                $resultsContainer.append($col);
-                    
-            });       
 
+            $col.append($card);
+
+            $cardImg.append(image);
+            $cardImg.append("<span class='card-title'>" + eventName + "</span>");
+            $cardContent.append("<p><b>Start : </b>" + eventStartIn + "<br>" + eventStartShort + "<b> at </b>" + eventStartTime + "</p>");
+            $cardContent.append("<p><b>End : </b><br>" + eventEndShort + "<b> at </b>" + eventEndTime + "</p>");
+            $cardContent.append("<p><b>Link : </b><span><a href='" + ebClick + "'>Go to Eventbrite</a></span>");
+            $cardContent.append("<p class='comment'><b>Description : </b>" + eventDesc + "</p>");
+            $(".comment").shorten();
             $card.append($cardImg);
             $card.append($cardContent);
 
             $resultsContainer.append($col);
+
         });
 
-    }
+        $card.append($cardImg);
+        $card.append($cardContent);
+
+        $resultsContainer.append($col);
+    });
+
+}
 
 // onclick function for searching the EVENTBRITE query
 $('#searchForm').submit(function (event) {
@@ -207,23 +215,23 @@ function runWeatherQuery() {
             var windSpeed = weatherEvent.speed;
             var humidity = weatherEvent.humidity;
 
-        var dateTimeString = moment.unix(date).format("MM-DD-YYYY")
-        console.log(dateTimeString);
+            var dateTimeString = moment.unix(date).format("MM-DD-YYYY")
+            console.log(dateTimeString);
 
-        // append results to table!
-        $(".tableArea").append("<tr>" +
-            "<td> " + dateTimeString +
-            "<td>" + minTemp + '°F' +
-            "<td>" + maxTemp + '°F' +
-            "<td>" + windSpeed + ' m/h'+
-            "<td>" + humidity +'%'+
-            "</td>");    
+            // append results to table!
+            $(".tableArea").append("<tr>" +
+                "<td> " + dateTimeString +
+                "<td>" + minTemp + '°F' +
+                "<td>" + maxTemp + '°F' +
+                "<td>" + windSpeed + ' m/h' +
+                "<td>" + humidity + '%' +
+                "</td>");
 
 
             // weather img icon
             var iconcode = weatherEvent.weather[0].icon;
-    var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-    console.log(iconurl);
+            var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+            console.log(iconurl);
 
 
         });
